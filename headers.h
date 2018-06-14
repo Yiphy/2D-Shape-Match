55e4ccdb340b4aa52f09fe56f7bee0ed5ac2677c
Zhang Yifei <yiphyzhang@126.com>
2018-06-14 22:38:10 +0800

    Create headers.h

---
+++ headers.h
@@ -0,0 +1,24 @@
+#ifndef HEADERS_H_
+#define HEADERS_H_
+
+#include<iostream>
+#include<limits>
+#include<math.h>
+#include<vector>
+#include<string>
+#include<time.h>
+#include<io.h>
+#include<tuple>
+#include<windows.h>
+#include<direct.h>  
+#include<algorithm>
+
+#include<cv.h>
+#include<highgui.h>
+#include<ml.h>
+#include<ppl.h>
+
+using namespace std;
+using namespace cv;
+
+#endif
